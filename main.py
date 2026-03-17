cf11ac89dec012fdefbe454e1897951e31d0822d
JUAN FELIPE INÁCIO <juan_vpa@hotmail.com>
2026-03-17 18:39:07 -0300

    corrigindo o main completo

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+from fastapi.responses import RedirectResponse
+from starlette.middleware.sessions import SessionMiddleware
 
 from routers import home
 from routers import admin
 
 app = FastAPI()
 
+app.add_middleware(SessionMiddleware, secret_key="senha_super_secreta")
+
 # templates
 templates = Jinja2Templates(directory="templates")
 
-# EXEMPLO (se não tiver ainda vindo do banco)
+# EXEMPLO (se não tiver banco ainda)
 lista_servicos = [
     ("1", "Design de sobrancelha", 40),
     ("2", "Design com henna", 50),
@@ -23,6 +27,22 @@ def agendamento(request: Request, servico: str = None):
         "request": request,
         "servicos": lista_servicos,
         "servico_selecionado": servico
+    })
+
+# LOGIN
+@app.get("/login")
+def login_page(request: Request):
+    return templates.TemplateResponse("login.html", {"request": request})
+
+@app.post("/login")
+def login(request: Request, senha: str = Form(...)):
+    if senha == "1234":
+        request.session["logado"] = True
+        return RedirectResponse("/admin", status_code=302)
+
+    return templates.TemplateResponse("login.html", {
+        "request": request,
+        "erro": "Senha incorreta"
     })
 
 # arquivos estáticos
