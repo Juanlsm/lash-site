c2da8db0b61ad0544a7a9847499e401f0bbd5361
JUAN FELIPE INÁCIO <juan_vpa@hotmail.com>
2026-03-17 18:29:29 -0300

    corrigindo rota agendamento

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 from routers import home
 from routers import admin
 
 app = FastAPI()
+
+# templates
+templates = Jinja2Templates(directory="templates")
+
+# EXEMPLO (se não tiver ainda vindo do banco)
+lista_servicos = [
+    ("1", "Design de sobrancelha", 40),
+    ("2", "Design com henna", 50),
+]
+
+# rota agendamento
+@app.get("/agendamento")
+def agendamento(request: Request, servico: str = None):
+    return templates.TemplateResponse("agendamento.html", {
+        "request": request,
+        "servicos": lista_servicos,
+        "servico_selecionado": servico
+    })
 
 # arquivos estáticos
 app.mount("/static", StaticFiles(directory="static"), name="static")
